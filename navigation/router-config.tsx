e8bdae643602795571790b39825eb0a30c37ab63
KAsare1 <asarek977@gmail.com>
2023-07-24 16:06:03 +0000

    Update router-config.tsx

--- navigation/router-config.tsx
+++ navigation/router-config.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "../screens/home";
-
+import HomeIcon from '@mui/icons-material/Home';
+import PersonIcon from '@mui/icons-material/Person';
 import { SafeAreaView } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Settings from "../screens/profile";
 import PresidentScreen from "../screens/President";
 import SecretaryScreen from "../screens/Secretary";
 import VicePresidentScreen from "../screens/vice";
+import { Icon, IconButton, SvgIcon } from "@mui/material";
 
 const Tab = createBottomTabNavigator();
 
@@ -35,9 +37,15 @@ const RouterConfig = () => {
 // MainScreen to include the Tab.Navigator
 const MainScreen = () => {
   return (
-    <Tab.Navigator screenOptions={{ headerShown: true }}>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Profile" component={Settings} />
+    <Tab.Navigator screenOptions={{ headerShown: true, tabBarActiveBackgroundColor: '#A7FFEB', tabBarActiveTintColor:'black' }}>
+      <Tab.Screen name="Home" component={Home} options={{tabBarLabel: 'Home', tabBarIcon: ({ color }) => (
+        <HomeIcon/>
+        ),
+      }} />
+      <Tab.Screen name="Profile" component={Settings} options={{tabBarLabel: 'Profile', tabBarIcon: ({ color }) => (
+        <PersonIcon/>
+        ),
+      }}/>
     </Tab.Navigator>
   );
 };
